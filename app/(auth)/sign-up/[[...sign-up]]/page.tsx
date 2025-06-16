74f87ee3e45ef67972094029425d79b05fd97578
Prathmesh-rajurkar <prathmeshrajurkar199@gmail.com>
2025-06-16 17:25:06 +0530

    clerk auth

---
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,34 @@
+import { SignUp } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
+
+export default function Page() {
+  return (
+    <div className="flex justify-center items-center flex-col gap-4">
+      <div>
+        <h1
+          style={{ fontFamily: "var(--font-belanosima)" }}
+          className="text-6xl  mb-6 text-center bg-gradient-to-r from-yellow-600 to-red-600 bg-clip-text text-transparent"
+        >
+          backendbro
+        </h1>
+      </div>
+      <div>
+        <SignUp
+          appearance={{
+            baseTheme: dark,
+            elements: {
+              card: {
+                backdropFilter: "blur(5px) saturate(180%)",
+                WebkitBackdropFilter: "blur(5px) saturate(180%)",
+                backgroundColor: "rgba(255, 255, 255, 0.05)",
+                borderRadius: "20px",
+                border: "1px solid rgba(255, 255, 255, 0.3)",
+                boxShadow: "0 8px 32px rgba(0, 0, 0, 0.1)",
+              }
+            },
+          }}
+        />
+      </div>
+    </div>
+  );
+}
